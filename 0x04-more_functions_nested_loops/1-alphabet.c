4e7d3f0321b15deb9d4d5abef1df3c52972c8d7d
pilipili404 <mathewanariko@gmail.com>
2023-09-15 20:36:52 -0700

    added stuff

---
+++ 0x04-more_functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet prints alphabet in lowercase
+ *
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	  putchar(c);
+	}
+	_putchar('\n');
+}
