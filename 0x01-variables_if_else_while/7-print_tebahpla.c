9644ab3483fde80dd54b3bc736ad8f5e52272e3a
pilipili404 <mathewanariko@gmail.com>
2023-09-18 22:53:11 -0700

    added stuff

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - it prints alphabets in lowercase(in reverse) followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
