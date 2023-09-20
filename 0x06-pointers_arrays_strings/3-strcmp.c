423adf2323bf28ebb0fb9c4d9e26e3817bb41e7d
pilipili404 <mathewanariko@gmail.com>
2023-09-20 14:51:35 -0700

    added some stuff

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcmp - Compares two strings
  * @s1: The first string to compare
@@ -8,19 +7,19 @@
  * Return: 0 if the strings are equal, a negative value if s1 < s2,
  *         or a positive value if s1 > s2
  */
+
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
+	int i;
 
-    while (s1[i] != '\0' || s2[i] != '\0')
-    {
-        if (s1[i] != s2[i])
-        {
-            return s1[i] - s2[i];
-        }
-        i++;
-    }
+	i = 0;
 
-    return 0; // Strings are equal
+	while ((s1[i] != '\0') || (s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (0);
 }
-
