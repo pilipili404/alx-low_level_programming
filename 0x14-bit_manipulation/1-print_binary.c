bdf8d8d8f2de39da9dfff2bae79eb74c3daa6f79
pilipili404 <mathewanariko@gmail.com>
2023-10-26 20:56:57 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,28 @@
+
 #include "main.h"
+
+/**
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
 
 /**
  * print_binary - prints a number in binary notation
  * @n: number to print
+ *
  * Return: void
  */
 void print_binary(unsigned long int n)
@@ -26,22 +46,5 @@ void print_binary(unsigned long int n)
 		}
 		divisor >>= 1;
 	}
-}
-
-/**
- * _pow - func calculates (base ^ power)
- * @base: base of the exponent
- * @power: power of the exponent
- * Return: value of (base ^ power)
- */
-unsigned long int _pow(unsigned int base, unsigned int power)
-{
-	unsigned long int num;
-	unsigned int a;
-
-	num = 1;
-	for (a = 1; a <= power; a++)
-		num *= base;
-	return (num);
 }
 
